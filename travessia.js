72452f5203e4858b9f6088bcf196550eb7d9486b
João Pereira <nashi.codes@gmail.com>
2022-07-28 02:30:47 -0300

    display das telas para trabalhar melhor com os elementos

---
+++ travessia.js
@@ -0,0 +1,7 @@
+let inicio = document.querySelector("#telaInicial");
+let vitoria = document.querySelector("#vitoria");
+let derrota = document.querySelector("#derrota");
+
+inicio.style.display = "none";
+vitoria.style.display = "none";
+derrota.style.display = "none";
