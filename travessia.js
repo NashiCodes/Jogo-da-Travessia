f90996bf46f83b5fe8df62631b668834d4a7147a
João Pereira <nashi.codes@gmail.com>
2022-07-29 19:23:51 -0300

    tela inicial acrescentada e estillizada

--- travessia.js
+++ travessia.js
@@ -2,6 +2,19 @@ let start = document.querySelector(".telaInicial");
 let vitoria = document.querySelector(".vitoria");
 let derrota = document.querySelector(".derrota");
 
-start.style.display = "none";
+let ovelha = document.querySelector("#ovelha");
+let lobo = document.querySelector("#lobo");
+let alface = document.querySelector("#alface");
+
+let farmer = document.querySelector("#farmer");
+let boat = document.querySelector("#boat");
+
 vitoria.style.display = "none";
 derrota.style.display = "none";
+
+ovelha.style.display = "none";
+lobo.style.display = "none";
+alface.style.display = "none";
+
+farmer.style.display = "none";
+boat.style.display = "none";
