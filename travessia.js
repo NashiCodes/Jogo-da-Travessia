78a27fefdac9dadea0ea82f1ca7afe461e509f8d
João Pereira <nashi.codes@gmail.com>
2022-07-29 17:18:29 -0300

    algoritmo refeito e adaptado para melhor implementação

--- travessia.js
+++ travessia.js
@@ -1,1 +1,7 @@
+let start = document.querySelector(".telaInicial");
+let vitoria = document.querySelector(".vitoria");
+let derrota = document.querySelector(".derrota");
 
+start.style.display = "none";
+vitoria.style.display = "none";
+derrota.style.display = "none";
