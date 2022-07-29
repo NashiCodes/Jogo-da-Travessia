49c3c30836c8582027ae1297d46eea7be2282d50
João Pereira <nashi.codes@gmail.com>
2022-07-29 16:41:10 -0300

    js sera refeito

--- travessia.js
+++ travessia.js
@@ -1,7 +1,1 @@
-let inicio = document.querySelector("#telaInicial");
-let vitoria = document.querySelector("#vitoria");
-let derrota = document.querySelector("#derrota");
 
-inicio.style.display = "none";
-vitoria.style.display = "none";
-derrota.style.display = "none";
