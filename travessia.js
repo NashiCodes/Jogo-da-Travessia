ae5a7ae37878a545d34074660b2eab2297a89b76
João Pereira <nashi.codes@gmail.com>
2022-07-29 23:35:59 -0300

    funcao para escutar o clique dois botoes implementado

--- travessia.js
+++ travessia.js
@@ -1,6 +1,10 @@
-let start = document.querySelector(".telaInicial");
-let vitoria = document.querySelector(".vitoria");
-let derrota = document.querySelector(".derrota");
+let telaInicial = document.querySelector("#telaInicial");
+let vitoria = document.querySelector("#vitoria");
+let derrota = document.querySelector("#derrota");
+
+let Esqr = document.querySelector(".Esqr");
+let Rio = document.querySelector(".Rio");
+let Dir = document.querySelector(".Dir");
 
 let ovelha = document.querySelector("#ovelha");
 let lobo = document.querySelector("#lobo");
@@ -8,6 +12,8 @@ let alface = document.querySelector("#alface");
 
 let farmer = document.querySelector("#farmer");
 let boat = document.querySelector("#boat");
+
+const botao = document.querySelector(".botao");
 
 vitoria.style.display = "none";
 derrota.style.display = "none";
@@ -18,3 +24,32 @@ alface.style.display = "none";
 
 farmer.style.display = "none";
 boat.style.display = "none";
+
+botao.addEventListener("click", chamafuncao);
+
+function chamafuncao(e) {
+  if (telaInicial.style.display == "") {
+    telaInicial.style.display = "none";
+    ovelha.style.display = "";
+    lobo.style.display = "";
+    alface.style.display = "";
+    farmer.style.display = "";
+    boat.style.display = "";
+
+    return 0;
+  }
+
+  reinicia();
+}
+
+function reinicia() {
+  leftSide.appendChild(ovelha);
+  leftSide.appendChild(lobo);
+  leftSide.appendChild(alface);
+  ovelha.style.position = "static";
+  lobo.style.position = "static";
+  alface.style.position = "static";
+
+  vitoria.style.display = "none";
+  derrota.style.display = "none";
+}
