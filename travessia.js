053e73904b7fa1fc28b5efba9d2242b6f932384d
João Pereira <nashi.codes@gmail.com>
2022-07-30 18:08:32 -0300

    modificação para os elementos serem criados pelo arquivo js

--- travessia.js
+++ travessia.js
@@ -6,21 +6,29 @@ let Esqr = document.querySelector(".Esqr");
 let Rio = document.querySelector(".Rio");
 let Dir = document.querySelector(".Dir");
 
-let ovelha = document.querySelector("#ovelha");
-let lobo = document.querySelector("#lobo");
-let alface = document.querySelector("#alface");
+let ovelha = document.createElement("img");
+let lobo = document.createElement("img");
+let alface = document.createElement("img");
 
 let farmer = document.querySelector("#farmer");
 let boat = document.querySelector("#boat");
 
 const botao = document.querySelector(".botao");
 
+ovelha.id = "ovelha";
+ovelha.className = "animal";
+ovelha.src = "./imgs/ovelha.png";
+
+lobo.id = "lobo";
+lobo.className = "animal";
+lobo.src = "./imgs/lobo.png";
+
+alface.id = "alface";
+alface.className = "animal";
+alface.src = "./imgs/lettuce.png";
+
 vitoria.style.display = "none";
 derrota.style.display = "none";
-
-ovelha.style.display = "none";
-lobo.style.display = "none";
-alface.style.display = "none";
 
 farmer.style.display = "none";
 boat.style.display = "none";
@@ -30,6 +38,9 @@ botao.addEventListener("click", chamafuncao);
 function chamafuncao(e) {
   if (telaInicial.style.display == "") {
     telaInicial.style.display = "none";
+    Esqr.appendChild(ovelha);
+    Esqr.appendChild(lobo);
+    Esqr.appendChild(alface);
     ovelha.style.display = "";
     lobo.style.display = "";
     alface.style.display = "";
@@ -43,9 +54,9 @@ function chamafuncao(e) {
 }
 
 function reinicia() {
-  leftSide.appendChild(ovelha);
-  leftSide.appendChild(lobo);
-  leftSide.appendChild(alface);
+  Esqr.appendChild(ovelha);
+  Esqr.appendChild(lobo);
+  Esqr.appendChild(alface);
   ovelha.style.position = "static";
   lobo.style.position = "static";
   alface.style.position = "static";
